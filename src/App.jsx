125a62707540f52a724484bf277d2d1c7093ae1f
Seung Bin Lee <leesb5510@gmail.com>
2024-05-16 10:38:33 +0900

    feat: 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,45 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const onClick = () => {
+    const newTodo = {
+      title,
+      description,
+      id: new Date().getTime(),
+      isDone: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  const onChangeTitle = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const onChangeDescription = (event) => {
+    setDescription(event.target.value);
+  };
+
+  const onDeleteTodo = (id) => {
+    const updateTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updateTodos);
+  };
+
+  const onChangeTodoState = (id) => {
+    const updateTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, isDone: !todo.isDone };
+      }
+      return todo;
+    });
+
+    setTodos(updateTodos);
+  };
+
   return (
     <div className="container">
       <header className="header">
@@ -10,36 +49,68 @@ function App() {
       <div className="writeForm">
         <div>
           <label htmlFor="title">제목</label>
-          <input type="text" id="title" />
+          <input type="text" id="title" onChange={onChangeTitle} />
           <label htmlFor="description">내용</label>
-          <input type="text" id="description" />
+          <input type="text" id="description" onChange={onChangeDescription} />
         </div>
-        <button type="button">작성하기</button>
+        <button type="button" onClick={onClick}>
+          작성하기
+        </button>
       </div>
       <section className="section">
         <h2>Working... 🔥</h2>
         <ul className="todos">
-          <li className="todo">
-            <strong>제목입니다</strong>
-            <p>설명입니다</p>
-            <div className="btn-wrap">
-              <button type="button">삭제하기</button>
-              <button type="button">완료</button>
-            </div>
-          </li>
+          {todos.map((todo) => {
+            if (todo.isDone) {
+              return;
+            }
+
+            return (
+              <li className="todo" key={todo.id}>
+                <strong>{todo.title}</strong>
+                <p>{todo.description}</p>
+                <div className="btn-wrap">
+                  <button type="button" onClick={() => onDeleteTodo(todo.id)}>
+                    삭제하기
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => onChangeTodoState(todo.id)}
+                  >
+                    완료
+                  </button>
+                </div>
+              </li>
+            );
+          })}
         </ul>
       </section>
       <section className="section">
         <h2>Done... 💬</h2>
         <ul className="todos">
-          <li className="todo">
-            <strong>제목입니다</strong>
-            <p>설명입니다</p>
-            <div className="btn-wrap">
-              <button type="button">삭제하기</button>
-              <button type="button">완료</button>
-            </div>
-          </li>
+          {todos.map((todo) => {
+            if (!todo.isDone) {
+              return;
+            }
+
+            return (
+              <li className="todo" key={todo.id}>
+                <strong>{todo.title}</strong>
+                <p>{todo.description}</p>
+                <div className="btn-wrap">
+                  <button type="button" onClick={() => onDeleteTodo(todo.id)}>
+                    삭제하기
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => onChangeTodoState(todo.id)}
+                  >
+                    완료
+                  </button>
+                </div>
+              </li>
+            );
+          })}
         </ul>
       </section>
     </div>
